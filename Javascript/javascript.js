609a7d759256ced1081e7a4170a5a3b3094ae19a
AshkanNadiri <alireza_nadiri70@yahoo.com>
2019-03-18 09:39:08 -0700

    logo updated/working on Css  & Html/

---
+++ Javascript/javascript.js
@@ -0,0 +1,9 @@
+// const button = document.createElement('button');
+// document.body.appendChild(button);
+// button.innerText = "Menu";
+// button.style.backgroundColor = 'rgba(64, 64, 64,50)';
+// var btn_clr = "white";
+// button.style.color = 'white'
+// button.style.fontSize = '30px';
+// button.style.border = 'none';
+// button.style.font = 'serif'
