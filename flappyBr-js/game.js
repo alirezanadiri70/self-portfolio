698153afd897d731dda25cae1c167c9c9f4e2f77
AshkanNadiri <alireza_nadiri70@yahoo.com>
2019-03-19 15:21:01 -0700

    working on port js

--- flappyBr-js/game.js
+++ flappyBr-js/game.js
@@ -1,8 +1,9 @@
-//canvas size//
-const birdCan = document.querySelector('.bird-can');
-birdCan.style.width = 1000;
-birdCan.style.height = 900;
-birdCan.style.backgroundColor = white;
-//Game canvas//
-const context = canvas.getContext('2d');
+// //canvas & img//
+const canvas = document.querySelector('canvas');
+
+
+
+
+// //Game canvas//
+// // const context = canvas.getContext('2d');
 
