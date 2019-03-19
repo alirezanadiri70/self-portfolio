606288f777a596baca30b05b848d8f99e2f423fb
AshkanNadiri <alireza_nadiri70@yahoo.com>
2019-03-19 10:03:03 -0700

    changes on js file and index

---
+++ flappyBr-js/game.js
@@ -0,0 +1,8 @@
+//canvas size//
+const birdCan = document.querySelector('.bird-can');
+birdCan.style.width = 1000;
+birdCan.style.height = 900;
+birdCan.style.backgroundColor = white;
+//Game canvas//
+const context = canvas.getContext('2d');
+
