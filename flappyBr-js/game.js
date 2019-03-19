49eedc4800ea2495727fad32fa7b3e17e46241c9
AshkanNadiri <alireza_nadiri70@yahoo.com>
2019-03-19 16:29:18 -0700

    working on to do list js

--- flappyBr-js/game.js
+++ flappyBr-js/game.js
@@ -1,9 +1,13 @@
-// //canvas & img//
-const canvas = document.querySelector('canvas');
+// // //canvas & img//
+// const canvas = document.querySelector('canvas');
+// const img = document.createElement('img');
+// img.src = 'flappyBr-js/sprite.png';
+// document.body.prepend(img);
+// // //Game canvas//
+// const context = canvas.getContext('2d');
 
 
-
-
-// //Game canvas//
-// // const context = canvas.getContext('2d');
+// function gameLoop(){
+    
+// }
 
---
+++ flappyBr-js/player.js
@@ -0,0 +1,6 @@
+// class Player{
+   
+//     draw(){
+//         context.drawImage(img, 0,0,100,100)
+//     }
+// }
