c175376eaa82f3849ae1fc2be4b3c9540aa9c43d
AshkanNadiri <alireza_nadiri70@yahoo.com>
2019-03-20 12:06:15 -0700

    working on btn color

---
+++ Javascript/todolist.js
@@ -0,0 +1,31 @@
+// const listInput = document.getElementsByClassName('input');
+// const button = document.getElementsByTagName('button');
+// const ul = document.getElementsByClassName('ul-list');
+
+// button.addEventListener('click', addToList);
+// console.log('button')
+// function addToList(){
+//     const li = document.createElement('li');
+//     ul.appendChild(li);
+
+//     li.innerText = listInput.value;
+//     listInput.value ='';
+// }
+// const list = document.getElementsByClassName('list');
+// const canvas = list;
+// const listInput = document.createElement('input');
+// document.body.appendChild(listInput);
+// const button = document.createElement('button');
+// document.body.appendChild(button);
+// button.innerText = 'Submit';
+// const ul = document.createElement('ul');
+// document.body.appendChild (ul);
+
+// button.addEventListener('click', addToList);
+// function addToList(){
+//     const li = document.createElement('li');
+//     ul.appendChild(li);
+
+//     li.innerText = listInput.value;
+//     listInput.value = '';
+// }
