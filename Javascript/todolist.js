1edaf2a566e1f6b1e6892303129a1e413c59c962
AshkanNadiri <alireza_nadiri70@yahoo.com>
2019-03-20 18:06:59 -0700

    to do list is working

--- Javascript/todolist.js
+++ Javascript/todolist.js
@@ -1,31 +1,14 @@
-// const listInput = document.getElementsByClassName('input');
-// const button = document.getElementsByTagName('button');
-// const ul = document.getElementsByClassName('ul-list');
+const listInput = document.querySelector('.input');
+const button = document.querySelector('.btn');
+const ul = document.querySelector('.ul-list');
 
-// button.addEventListener('click', addToList);
-// console.log('button')
-// function addToList(){
-//     const li = document.createElement('li');
-//     ul.appendChild(li);
+button.addEventListener('click', addToList);
 
-//     li.innerText = listInput.value;
-//     listInput.value ='';
-// }
-// const list = document.getElementsByClassName('list');
-// const canvas = list;
-// const listInput = document.createElement('input');
-// document.body.appendChild(listInput);
-// const button = document.createElement('button');
-// document.body.appendChild(button);
-// button.innerText = 'Submit';
-// const ul = document.createElement('ul');
-// document.body.appendChild (ul);
-
-// button.addEventListener('click', addToList);
-// function addToList(){
-//     const li = document.createElement('li');
-//     ul.appendChild(li);
-
-//     li.innerText = listInput.value;
-//     listInput.value = '';
-// 
+function addToList(){
+    const li = document.createElement('li');
+    ul.appendChild(li);
+    
+    li.innerText = listInput.value;
+    listInput.value ='';
+    li.style.color ='white';
+}
