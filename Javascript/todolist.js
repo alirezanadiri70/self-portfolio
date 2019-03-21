b11fcea74474c5fc588ce47bc4200be6b28ff931
AshkanNadiri <alireza_nadiri70@yahoo.com>
2019-03-21 15:41:53 -0700

    working on color

--- Javascript/todolist.js
+++ Javascript/todolist.js
@@ -1,14 +1,25 @@
 const listInput = document.querySelector('.input');
 const button = document.querySelector('.btn');
 const ul = document.querySelector('.ul-list');
-
+const resetButton = document.querySelector('.re-btn');
+// const li = document.querySelector('.demo')
 button.addEventListener('click', addToList);
 
 function addToList(){
     const li = document.createElement('li');
     ul.appendChild(li);
-    
+    if (li.length === 10){
+        li.innerHTML = '';
+        console.log(li.length)
+    }
     li.innerText = listInput.value;
     listInput.value ='';
     li.style.color ='white';
 }
+// resetButton.addEventListener('click', clearList);
+// function clearList(){
+//     // window.confirm("Are you sure?");
+//     if (li.length === 1){
+//        console.log(li)
+//     }
+        
